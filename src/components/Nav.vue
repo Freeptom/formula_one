57cf5d224c54533ea71d79e5db1581a831a2783d
Tom Pearson <tom.pearson@vm94d9.ahc.uk.com>
2019-11-11 14:31:31 +0000

    add top to fixed nav

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -29,8 +29,10 @@ export default {
 .main-nav {
     background: white;
     margin: 0;
-    width: 5rem;
+    width: 8rem;
     position:fixed;
+    top: 0;
+    left: 0;
     border-right: 1px solid $gray2;
     height: 100%;
      height: 100%;
@@ -39,6 +41,10 @@ export default {
     list-style: none;
     margin: 0;
     padding: 0;
+  }
+  li {
+    padding: 2rem 0;
+    border-bottom: 1px solid $gray2;
   }
 
 }
