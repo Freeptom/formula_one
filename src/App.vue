ab42df4fa24c338dd49b94efba095cdedb9de9d4
Tom Pearson <tom.pearson@vm94d9.ahc.uk.com>
2019-11-20 09:00:48 +0000

    styling using grid

--- src/App.vue
+++ src/App.vue
@@ -21,30 +21,34 @@ export default {
 };
 </script>
 
-<style>
+<style scoped lang="scss">
 #app {
+  width: 100%;
   height: 100%;
   margin: 0;
-  display: grid;
   grid-gap: 2rem;
-  grid-template-columns: 8rem repeat( auto-fit, minmax(250px, 800px) );
+  
+  
+  @media screen and (min-width: 750px) {
+ display: grid;
+    grid-template-columns: 8rem repeat( auto-fit, minmax(350px, 650px) );
+    grid-template-rows: 4rem auto;
+    .main-nav {
+    grid-column: 1 ;
+    grid-row: 1 / -1;
+   }
+    .standings-module {
+    grid-column: 2;
+    grid-row: 2;
+    }
+    .calendar-module {
+    grid-row: 2;
+    grid-column: 3;
 
-
+    }
+  }
 }
 
-.main-nav {
-  grid-column: 1 ;
-  grid-row: 1 / -1;
-}
-
-.standings-module {
-  grid-column: 2;
-
-}
-.calendar-module {
-  grid-column: 3;
-
-}
 
 main {
   padding: 0 1rem;
