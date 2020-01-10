0a94c73d2a49329e0122bdbc77371a50b0f0d18a
Freeptom <32514405+Freeptom@users.noreply.github.com>
2020-01-10 11:28:17 +0000

    add driver routes, move main content from app to own component

---
+++ src/routes.js
@@ -0,0 +1,7 @@
+import dashboard from './components/Dashboard.vue';
+import showDrivers from './components/Drivers.vue';
+
+export default [
+    { path: '/', component: dashboard },
+    { path: '/drivers', component: showDrivers },
+];
