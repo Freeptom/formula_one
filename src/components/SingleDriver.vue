7f2ab1b7ee5c51163b7ab8c1e671a6c33d627f28
Freeptom <32514405+Freeptom@users.noreply.github.com>
2020-02-15 18:24:44 +0000

    WIP, get grid and finish results for driver, for each race

--- src/components/SingleDriver.vue
+++ src/components/SingleDriver.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex';
+import { mapState, mapGetters, mapActions } from 'vuex';
 // eslint-disable-next-line no-unused-vars
 import styles from '../styles/styles.scss';
 
@@ -15,17 +15,34 @@ export default {
   data() {
     return {
       driverId: this.$route.params.driver,
+      gridPlacement: [],
+      finishPlacement: [],
     };
   },
 
   computed: {
-    ...mapGetters(['driver']),
+    ...mapState(['races']),
+    ...mapGetters(['driver', 'allRaces', 'results']),
   },
 
   created() {
     this.fetchDrivers(this.driverId);
+    this.fetchRaces();
+    this.getPlacements();
   },
-  methods: { ...mapActions(['fetchDrivers']) },
+
+  methods: {
+    ...mapActions(['fetchDrivers', 'fetchRaces', 'fetchRoundResults']),
+    async getPlacements() {
+      for await (let el of this.allRaces) {
+        this.fetchRoundResults(el).then(() => console.log(this.results));
+      }
+
+      // request each race in allRaces?
+
+      // add grid and finish place to data arrays for each
+    },
+  },
 };
 </script>
 
