d206debda74ed1ffb787d54af595448060bc707c
Freeptom <32514405+Freeptom@users.noreply.github.com>
2020-02-15 17:00:41 +0000

    add single driver route

---
+++ src/components/SingleDriver.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>{{ driver }}</h1>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+// eslint-disable-next-line no-unused-vars
+import styles from '../styles/styles.scss';
+
+export default {
+  name: 'SingleDriver',
+
+  data() {
+    return {
+      driverId: this.$route.params.driver,
+    };
+  },
+
+  computed: {
+    ...mapGetters(['driver']),
+  },
+
+  created() {
+    this.fetchDrivers(this.driverId);
+  },
+  methods: { ...mapActions(['fetchDrivers']) },
+};
+</script>
+
+<style scoped lang="scss"></style>
